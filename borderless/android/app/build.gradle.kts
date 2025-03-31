909e4efcbb82504501d1fb3a7c2a355a134aa630
yusufmolumo <y.molumo@alustudent.com>
2025-03-31 09:37:36 +0200

    appbuildgradleupdate

--- borderless/android/app/build.gradle.kts
+++ borderless/android/app/build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+
+    implementation("com.google.firebase:firebase-analytics")
+}
+
 android {
     namespace = "com.borderless.borderless"
     compileSdk = flutter.compileSdkVersion
